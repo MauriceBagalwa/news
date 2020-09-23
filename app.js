05b9f3675d449daeb3b8def3c9cb95b1b2412860
@The Sniper <mauricebagalwaa009@gmail.com>
2020-09-23 07:21:10 +0200

    Project initialization

---
+++ app.js
@@ -0,0 +1,22 @@
+/*
+ Importatins des packages importants
+*/
+const express = require("express");
+require("dotenv").config();
+/*
+ Mes variables
+*/
+const app = express();
+const PORT = process.env.PORT;
+/*
+ Home page
+*/
+app.get("/", (req, res) => {
+  res.send("welcome in BBC news...");
+});
+/*
+ Server
+*/
+app.listen(PORT, () => {
+  console.log(`Server is runing on port 🚀 http://localhost:${PORT}`);
+});
